96ad64b160e8041705ac10ac3c003109c7d8a2df
Alan-Bi <alan.bi326@gmail.com>
2018-08-31 12:42:49 -0700

    Add fields to contact page

--- Continents/ContactPage.xaml.cs
+++ Continents/ContactPage.xaml.cs
@@ -16,7 +16,12 @@ namespace Continents
 
         void OnSubmit()
         {
-            if (String.IsNullOrEmpty(emailEntry.Text) ||
+            if (String.IsNullOrEmpty(firstNameEntry.Text) ||
+                String.IsNullOrEmpty(lastNameEntry.Text) ||
+                String.IsNullOrEmpty(phoneEntry.Text) ||
+                String.IsNullOrEmpty(countryEntry.Text) ||
+                String.IsNullOrEmpty(cityEntry.Text) ||
+                String.IsNullOrEmpty(emailEntry.Text) ||
                 String.IsNullOrEmpty(subjectEntry.Text) ||
                 String.IsNullOrEmpty(bodyEntry.Text))
             {
@@ -27,12 +32,23 @@ namespace Continents
                 var mail = DependencyService.Get<IEmailSender>();
                 try
                 {
-                    mail.SendEmail("Mobile Contact Form: " + subjectEntry.Text, "Email: " + emailEntry.Text + "\n\n" + bodyEntry.Text);
+                    mail.SendEmail("Mobile Contact Form Subject: " + subjectEntry.Text, 
+                                   "Email: " + emailEntry.Text + "\n" +
+                                   "First Name: " + firstNameEntry.Text + "\n" +
+                                   "Last Name: " + lastNameEntry.Text + "\n" +
+                                   "Phone: " + phoneEntry.Text + "\n" +
+                                   "Country: " + countryEntry.Text + "\n" + 
+                                   "City: " + cityEntry.Text + "\n\n" + bodyEntry.Text);
                 }
                 catch (Exception e)
                 {
                     DisplayAlert("Error", "Unable to send contact form. Please try again later. ", "OK");
                 }
+                firstNameEntry.Text = String.Empty;
+                lastNameEntry.Text = String.Empty;
+                phoneEntry.Text = String.Empty;
+                countryEntry.Text = String.Empty;
+                cityEntry.Text = String.Empty;
                 emailEntry.Text = String.Empty;
                 subjectEntry.Text = String.Empty;
                 bodyEntry.Text = String.Empty;
