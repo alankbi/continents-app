090884ddcfe4ca99379456a665e3e6910d24c3ed
Alan-Bi <alan.bi326@gmail.com>
2018-08-30 17:00:54 -0700

    Remove scrollbar, disable all caps, and remove lines from text fields

---
+++ Droid/CustomEditorRenderer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Continents.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Android.Graphics.Drawables;
+using Android.Text;
+using Android.Content.Res;
+
+[assembly: ExportRenderer(typeof(Editor), typeof(CustomEditorRenderer))]
+namespace Continents.Droid
+{
+    public class CustomEditorRenderer : EditorRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                GradientDrawable gd = new GradientDrawable();
+                gd.SetColor(global::Android.Graphics.Color.Transparent);
+                this.Control.SetBackgroundDrawable(gd);
+                this.Control.SetRawInputType(InputTypes.TextFlagNoSuggestions);
+                Control.SetHintTextColor(ColorStateList.ValueOf(global::Android.Graphics.Color.Gray));
+            }
+        }
+    }
+}
