62d3d041d95c21d6856ac8749e364ba0c78b4a21
Alan-Bi <alan.bi326@gmail.com>
2018-09-01 17:43:22 -0700

    Update MailSender email info, disable FAQ page

--- iOS/MailSender.cs
+++ iOS/MailSender.cs
@@ -10,14 +10,15 @@ namespace Continents.iOS
     {
         public void SendEmail(string subject, string body)
         {
-            MailMessage mm = new MailMessage("orsonfakeemail@gmail.com", "custserv@continents.us");
+            MailMessage mm = new MailMessage("continentsmobilecontact@gmail.com", "custserv@continents.us");
             mm.Subject = subject;
             mm.Body = body;
 
             SmtpClient smtp = new SmtpClient();
             smtp.Host = "smtp.gmail.com";
             smtp.UseDefaultCredentials = false;
-            smtp.Credentials = new System.Net.NetworkCredential("orsonfakeemail@gmail.com", "oronsonon");
+            // to work, the email below must have security settings edited to allow access from less secure apps 
+            smtp.Credentials = new System.Net.NetworkCredential("continentsmobilecontact@gmail.com", "*****");
             smtp.EnableSsl = true;
             smtp.Port = 587;
             smtp.Send(mm);
